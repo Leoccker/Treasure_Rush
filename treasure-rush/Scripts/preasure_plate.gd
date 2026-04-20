2fd58140f3f19ca3fddf8e525f03c38e6c5a05a0
ChristianGCa <candelonichristian@gmail.com>
2026-04-20 13:24:09 -0300

    Adicionando placa de pressão funcional

--- treasure-rush/Scripts/preasure_plate.gd
+++ treasure-rush/Scripts/preasure_plate.gd
@@ -13,9 +13,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _body_entered_on_lever(body: Node2D) -> void:
+func _body_entered_on_preasure_plate(body: Node2D) -> void:
 	if frame == 0:
-		if body.name == "Player":
+		if body.name == "Rock":
 			frame = 1
 			sound.play()
 			emit_signal("preasure_plate_on")
